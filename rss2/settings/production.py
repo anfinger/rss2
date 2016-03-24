36d5c10cf659598412693be0688392beab660f58
anfinger <finger.a@gmx.de>
2016-03-24 11:56:58 +0100

    hmmmmmmmmmm

--- rss2/settings/production.py
+++ rss2/settings/production.py
@@ -3,7 +3,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
