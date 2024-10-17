7b78f62d3903ab4f47a1cec4d91f6e6efbe494bb
Andreas Finger <a.finger@reiseservice-schwerin.de>
2024-10-17 14:07:15 +0200

    2. Versuch Katalogzugehörigkeit Titel in Dropdown alphabetisch ordnen

--- reisen/admin.py
+++ reisen/admin.py
@@ -131,6 +131,11 @@ class ReisekatalogzugehoerigkeitInline(GrappelliSortableHiddenMixin, admin.Tabul
     fields = ('position', 'katalog_id', 'katalogseite', 'anzahl_seiten_im_katalog', 'position_auf_seite', 'titel', 'katalog_pdf')
     classes = ('grp-collapse grp-closed',)
     extra = 0
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+      if db_field.name == "katalog_id":
+        kwargs["queryset"] = Katalog.objects.order_by('titel')
+        return super(ReisekatalogzugehoerigkeitInline, self).formfield_for_foreignkey(db_field, request, **kwargs)    
 
 class ReisekategorienInline(GrappelliSortableHiddenMixin, admin.TabularInline):
     model = Reisekategorien
